f8698a9f883c0d8898a6317fccc1793a625ee93f
Ethan Snyder <techrules08041@yahoo.com>
2018-11-01 09:30:24 -0500

    added base files for Midi channel editing plugin

---
+++ include/plugins/midi_edit.h
@@ -0,0 +1,13 @@
+#ifndef CORE_PLUGINS_MIDI_EDIT_H_
+#define CORE_PLUGINS_MIDI_EDIT_H_
+
+#include <core/plugin.h>
+#include <core/midi.h>
+
+#include <metadata/plugins.h>
+
+using namespace lsp;
+
+
+
+#endif /* CORE_PLUGINS_MIDI_EDIT_H_ */
---
+++ src/plugins/midi_edit.cpp
@@ -0,0 +1,17 @@
+/*************************************************
+ * Midi Editor                                   *
+ * Created by: Ethan Snyder                      *
+ * Created on: 11 November, 2018                 *
+ *************************************************/
+
+ #include <dsp/dsp.h>
+
+ #include <core/debug.h>
+ #include <plugins/midi_edit.h>
+ #include <math.h>
+
+ #include <string.h>
+
+ using namespace lsp;
+
+ 
