b46626862a0cf32289b363908299874a675a2d53
Ethan Snyder <techrules08041@yahoo.com>
2018-11-01 09:43:34 -0500

    Worked on metadata header file for the midi edit override plugin

--- include/metadata/midi_edit.h
+++ include/metadata/midi_edit.h
@@ -1,4 +1,14 @@
 #ifndef METADATA_MIDI_EDIT_H_
 #define METADATA_MIDI_EDIT_H_
 
+struct midi_edit_metadata: public midi_edit_base_metadata
+{
+    static const plugin_metadata_t metadata;
+
+    enum ports
+    {
+        
+    }
+}
+
 #endif /* METADATA_MIDI_EDIT_H_ */
